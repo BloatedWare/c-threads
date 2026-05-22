657e559ef680c61104a4edc6e198732117c431d0
TheStriker <anasrifakofficial@gmail.com>
2026-05-23 00:50:43 +0100

    learned a few thing
    -turns out the gcc compiler does NOT care about what u give a no args function, meaning i can give it whatever but it won't complain or malfunction
    -thread entry points/functions have a specific type but still work anyway, idk what is going on

--- src/main.c
+++ src/main.c
@@ -3,26 +3,25 @@
 #include <unistd.h>
 
 
-void printText(void *arg);
+void printText();
 
 int main() {
-
-    int x = 29;
-    int y = 468;
+    
+    // int x = 29;
+    // int y = 468;
     pthread_t thread1;
     pthread_t thread2;
     
-    pthread_create(&thread1, NULL, (void*)&printText, (void*)&x);
-    pthread_create(&thread2, NULL, (void*)&printText, (void*)&y);
+    pthread_create(&thread1, NULL, (void (*)())&printText, (void*)NULL);
+    pthread_create(&thread2, NULL, (void (*)())&printText, (void*)NULL);
 
 
     pthread_join(thread1, NULL);
     return 0;
 }
 
-void printText(void* arg) {
+void printText() {
     static int counter = 0;
-    int cast_arg = *(int*)arg;//I m creating copy
     int local_counter;
     // if(counter%2) {
     //     printf("counter == %d \n", counter);
@@ -30,12 +29,13 @@ void printText(void* arg) {
     // }
     local_counter = ++counter;
     
-    printf("the passed arg to function via pthread_create (before): %d\n", *(int*)arg);
-    *(int*)arg *= 10;
-    printf("the passed arg to function via pthread_create (after): %d\n", *(int*)arg);
+    // printf("the passed arg to function via pthread_create (before): %d\n", *(int*)arg);
+    // *(int*)arg *= 10;
+    // printf("the passed arg to function via pthread_create (after): %d\n", *(int*)arg);
 
     while (1) {
         printf("Hello there!%d\n", local_counter);
         sleep(1);
     }
+
 }
