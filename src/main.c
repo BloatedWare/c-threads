b2cb563addfecbc383c1d538021ca852cce6c468
TheStriker <anasrifakofficial@gmail.com>
2026-05-23 23:46:58 +0100

    what we learned so far
    -so what we learned is, using the same pthread_t (which is a hidden pthread struct pointer) variable in pthread_create,it simply acts as overriding a pointer
    meaning, the thread still does its thing but u can no longer use join on it, and maybe other thread operation
    -calling join a thread that already finished executing will lead it the pthread_join() to return successfully
    -calling it on a uninitialized p_thread will lead to undefined behavior
    -the entry function that is given to a thread returns takes void* so that it is able to take ANY argument, you just take the address and access it for what it is
    -the same entry function returns a generic address that will point to something in HEAP, which is why pthread_join() takes the address of a generic pointer, so that it could store that address in your local variable

--- src/main.c
+++ src/main.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 
 void* printText(void* seconds);
 
 int main() {
-    
-    int s1 = 10, s2 = 15;
+    void *my_thing = NULL, *my_thing2 = NULL;
+    int s1 = 5, s2 = 8;
     pthread_t thread1;
     pthread_t thread2;
     
     pthread_create(&thread1, NULL, (void* (*)(void*))&printText, (void*)&s1);
+    sleep(3);
     pthread_create(&thread2, NULL, (void* (*)(void*))&printText, (void*)&s2);
     
 
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
+    pthread_join(thread1, &my_thing);
+    printf("thread1 returned %p and i will read it as (int) because i can :p\nmy thing = %d\n",my_thing, *(int*)my_thing);
+    pthread_join(thread2, &my_thing2);
+    printf("thread2 returned %p and i will read it as (int) because i can :p\nmy thing 2= %d\n",my_thing, *(int*)my_thing2);
+    free(my_thing);
+    free(my_thing2);
     return 0;
 }
 
 void* printText(void* seconds) {
     int secs = *(int*)seconds;    //copy value
+    int *someHeapAddress;
     for (int i = 1; i <= secs; i++) {
         printf("i= %d\n", i);
         sleep(1);
     }
-
+    someHeapAddress = (int*)malloc(sizeof(int));
+    if(someHeapAddress == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    *someHeapAddress = secs;
 }
