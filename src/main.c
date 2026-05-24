098aa41aaebda7efa95609280b721f2357dd4623
TheStriker <anasrifakofficial@gmail.com>
2026-05-24 04:12:51 +0100

    implemented mutexes
    - the way the mutexe locks work (MUTual EXEclusion locks) is, think of them as a variable
    that pthread_mutexe_lock() function checks and if it's not set (unlocked, it sets it(0->1 aka locks it) and proceeds
    else, the thread will sleep at that line of code until pthread_mutex_unlock() sees the variable is set, unsets it, checks if there are threads waiting in a queue, wakes the first one and moves on to next line.
    - 2 locks on the same lock by the same thread causes a self deadlock
    - 2 unlocks causes undefined behavior, i tested it, but luckily nothing happened.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 
 int counter = 0;
+pthread_mutex_t myLock = PTHREAD_MUTEX_INITIALIZER;
 
 void *thread_entry(void *arg);
 
@@ -13,19 +14,26 @@ int main() {
 
     pthread_create(&thread1, NULL, (void* (*)(void*))thread_entry, (void*)&th1_max);
     pthread_create(&thread2, NULL, (void* (*)(void*))thread_entry, (void*)&th2_max);
-    pthread_join(thread1, (void*)&th1_ret);
-    pthread_join(thread2, (void*)&th2_ret);
-    printf("th1_ret, th2_ret values= %p, %p\n", th1_ret, th2_ret);
-    printf("th1= %d, th2= %d\n", th1_max, th2_max);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    // for (int i = 0; i < th1_max; i++) {
+    //     counter++;
+    // }
+    // for (int i = 0; i < th2_max; i++) {
+    //     counter++;
+    // }
     printf("counter= %d\n", counter);
     return 0;
 }
 
 void *thread_entry(void *arg) {
     //args will be a pointer to an int
+
     int max_count = *(int*)arg;
+    pthread_mutex_lock(&myLock);
     for (int i = 0; i < max_count; i++) {
         counter++;
     }
-    
+    pthread_mutex_unlock(&myLock);
+    return NULL;//now this no longer undefined behavior   
 }
