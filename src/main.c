e0ff6b21f4e3ca02b978e7fce1f806e535757a78
TheStriker <anasrifakofficial@gmail.com>
2026-05-24 03:24:28 +0100

    another snapshot
    -turns out gcc doesn't warn you that you forgot to return something in C and if u try to retrieve what the function returned
    it just retrieves whatever lives in eax register (this is in assembly level) which is a garbage value,
    tested it with int, double and returned random value (0xc for void*, (the first test), 12 for int and 0.0000 for double)
    - current code is not thread safe, because 2 threads are "racing" to read and write on the global variable (counter)
    - next commit will have mutex locks implemented (WOOOOO)

--- src/main.c
+++ src/main.c
@@ -1,41 +1,31 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <unistd.h>
-#include <stdlib.h>
 
-void* printText(void* seconds);
+int counter = 0;
+
+void *thread_entry(void *arg);
 
 int main() {
-    void *my_thing = NULL, *my_thing2 = NULL;
-    int s1 = 5, s2 = 8;
-    pthread_t thread1;
-    pthread_t thread2;
-    
-    pthread_create(&thread1, NULL, (void* (*)(void*))&printText, (void*)&s1);
-    sleep(3);
-    pthread_create(&thread2, NULL, (void* (*)(void*))&printText, (void*)&s2);
-    
+    pthread_t thread1, thread2;
+    int th1_max = 1000000;
+    int th2_max = 2000000;
+    int *th1_ret, *th2_ret;
 
-    pthread_join(thread1, &my_thing);
-    printf("thread1 returned %p and i will read it as (int) because i can :p\nmy thing = %d\n",my_thing, *(int*)my_thing);
-    pthread_join(thread2, &my_thing2);
-    printf("thread2 returned %p and i will read it as (int) because i can :p\nmy thing 2= %d\n",my_thing, *(int*)my_thing2);
-    free(my_thing);
-    free(my_thing2);
+    pthread_create(&thread1, NULL, (void* (*)(void*))thread_entry, (void*)&th1_max);
+    pthread_create(&thread2, NULL, (void* (*)(void*))thread_entry, (void*)&th2_max);
+    pthread_join(thread1, (void*)&th1_ret);
+    pthread_join(thread2, (void*)&th2_ret);
+    printf("th1_ret, th2_ret values= %p, %p\n", th1_ret, th2_ret);
+    printf("th1= %d, th2= %d\n", th1_max, th2_max);
+    printf("counter= %d\n", counter);
     return 0;
 }
 
-void* printText(void* seconds) {
-    int secs = *(int*)seconds;    //copy value
-    int *someHeapAddress;
-    for (int i = 1; i <= secs; i++) {
-        printf("i= %d\n", i);
-        sleep(1);
+void *thread_entry(void *arg) {
+    //args will be a pointer to an int
+    int max_count = *(int*)arg;
+    for (int i = 0; i < max_count; i++) {
+        counter++;
     }
-    someHeapAddress = (int*)malloc(sizeof(int));
-    if(someHeapAddress == NULL) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-    *someHeapAddress = secs;
+    
 }
